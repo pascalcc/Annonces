847e1c62511bb3993a42242c5c53df0e58d8f5f6
PascalCC <costacunha@gmail.com>
2024-12-08 12:25:30 +0100

    Initial Commit

---
+++ GeevTT/GeevTTApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeevTTApp.swift
+//  GeevTT
+//
+//  Created by Pascal Costa-Cunha on 08/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct GeevTTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GeevTTTests/GeevTTTests.swift
@@ -0,0 +1,17 @@
+//
+//  GeevTTTests.swift
+//  GeevTTTests
+//
+//  Created by Pascal Costa-Cunha on 08/12/2024.
+//
+
+import Testing
+@testable import GeevTT
+
+struct GeevTTTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
